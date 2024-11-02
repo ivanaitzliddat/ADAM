c2c41787bbe658932e68d47876303cd648ea48fc
ivanaitzliddat <ivanaitzliddat@gmail.com>
2024-11-02 19:42:04 +0800

    Create main.py.

--- Actual/main.py
+++ Actual/main.py
@@ -1,6 +1,8 @@
-import screen_capturer
+from screen_capturer import ScreenCapturer
 
 if __name__ == "__main__":
-    device_count = 2
+    device_count = 1
     save_folder = "./screenshots"
-    ss_object = screen_capturer(save_folder, device_count)
+    ss_object = ScreenCapturer(save_folder, device_count)
+    ss_object.update_available_devices()
+    ss_object.capture_screenshots()
