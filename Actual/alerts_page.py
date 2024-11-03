98f5bede3a122f32fec838db1b612f7366754306
ivanaitzliddat <ivanaitzliddat@gmail.com>
2024-11-03 21:15:49 +0800

    Add alerts_page.py.

---
+++ Actual/alerts_page.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+class AlertsPage(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        label = tk.Label(self, text="Alerts Page Content", font=("Arial", 20))
+        label.pack(pady=20, padx=20)
