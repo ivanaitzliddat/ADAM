d6c55a3826a99493a427ae6f635895b006c1e8ba
ivanaitzliddat <ivanaitzliddat@gmail.com>
2024-11-03 20:10:28 +0800

    Update main.py to run both GUI and the screen capturer.

--- Actual/main.py
+++ Actual/main.py
@@ -1,8 +1,11 @@
 from screen_capturer import ScreenCapturer
+from gui import ADAM
 
 if __name__ == "__main__":
     device_count = 1
     save_folder = "./screenshots"
+    app = ADAM()
+    app.run()
     ss_object = ScreenCapturer(save_folder, device_count)
     ss_object.update_available_devices()
     ss_object.capture_screenshots()
