fb823784e9b3683c05912e3d1fd8fa4dfa3880c3
ivanaitzliddat <ivanaitzliddat@gmail.com>
2024-11-03 20:06:16 +0800

    Update gui.py to prevent for self-containment.

--- Actual/gui.py
+++ Actual/gui.py
@@ -4,10 +4,10 @@ import tkinter as tk
     Represents a GUI object which allows user to control ADAM.
 '''
 class ADAM:
-    def __init__(self, master):
-        self.master = master
+    def __init__(self):
+        self.master = tk.TK()
         self.master.title("ADAM")
 
         # Add a label
-        self.label = tk.label(master, text = "Welcome to ADAM!")
+        self.label = tk.label(self.master, text = "Welcome to ADAM!")
         self.label.pack()
