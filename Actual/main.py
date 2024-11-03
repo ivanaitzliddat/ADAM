24c69b76c722a3f7b543d78c7f120b646d6b1ba6
ivanaitzliddat <ivanaitzliddat@gmail.com>
2024-11-03 21:58:41 +0800

    Add comments to main.py

--- Actual/main.py
+++ Actual/main.py
@@ -3,14 +3,21 @@ from gui import ADAM
 
 device_count = 0
 
+'''
+    Updates the global variable device_count.
+'''
 def update_device_count(new_count):
     global device_count
     device_count = new_count
 
 if __name__ == "__main__":
     save_folder = "./screenshots"
+
+    # Create the ADAM GUI application
     app = ADAM(update_device_count)
     app.run()
+
+    # Start the screen capturer
     ss_object = ScreenCapturer(save_folder, device_count)
     ss_object.update_available_devices()
     ss_object.capture_screenshots()
