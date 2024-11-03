7eb10769724168d96ea855cb9d4e5302da8370c1
ivanaitzliddat <ivanaitzliddat@gmail.com>
2024-11-03 20:03:33 +0800

    Add comments for the gui class.

--- Actual/gui.py
+++ Actual/gui.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 
-class CaptureApp:
+'''
+    Represents a GUI object which allows user to control ADAM.
+'''
+class ADAM:
     def __init__(self, master):
         self.master = master
         self.master.title("ADAM")
 
         # Add a label
-        self.label = tk.label(master, text = "Welcome to ADAM")
+        self.label = tk.label(master, text = "Welcome to ADAM!")
         self.label.pack()
