9cf51f97d9a76ab64170b54a05c7c897eec8bee4
ivanaitzliddat <ivanaitzliddat@gmail.com>
2024-12-02 12:22:53 +0800

    Update ports testing script.

--- Actual/Hardware_Testing/identify_ports.py
+++ Actual/Hardware_Testing/identify_ports.py
@@ -1,4 +1,5 @@
 import subprocess
+import cv2
 
 def get_usb_video_devices():
     # Run the 'wmic' command to list all USB devices that are of type 'video capture'
@@ -12,20 +13,21 @@ def get_usb_video_devices():
     # The first line is a header, so we skip it
     # The rest are the devices that match the filter
     video_devices = [device for device in devices if device.strip() != '']
+
+    for i in video_devices:
+        cap = cv2.VideoCapture(i)
+
+        if not cap.isOpened():
+            print(f"Error: Could not open device {i}")
+
+        # Capture one frame
+        ret, frame = cap.read()
+
+        if ret:
+            image_path = "C:\\Users\\ivana\\Downloads\\image.png"
     
     return len(video_devices) - 1   # Subtract 1 to exclude the header line
 
 # Call the function and print the result
 usb_video_ports = get_usb_video_devices()
 print(f"Total USB cameras or video capture devices plugged in: {usb_video_ports}")
-
-cap = cv2.VideoCapture(i)
-
-if not cap.isOpened():
-    print(f"Error: Could not open device {i}")
-
-# Capture one frame
-ret, frame = cap.read()
-
-if ret:
-    image_path = "C:\\Users\\ivana\\Downloads\\"
