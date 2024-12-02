50244996f84b5309a0c10826bcd37c26c9e2f329
ivanaitzliddat <ivanaitzliddat@gmail.com>
2024-12-02 11:06:41 +0800

    Add ports identification script.

---
+++ Actual/Hardware_Testing/identify_ports.py
@@ -0,0 +1,31 @@
+import subprocess
+
+def get_usb_video_devices():
+    # Run the 'wmic' command to list all USB devices that are of type 'video capture'
+    result = subprocess.run(['wmic', 'path', 'Win32_PnPEntity', 'where', "Name like '%camera%' or Name like '%Display capture%'"], 
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # Decode the result and split by lines
+    devices = result.stdout.decode().splitlines()
+    print(devices)
+    
+    # The first line is a header, so we skip it
+    # The rest are the devices that match the filter
+    video_devices = [device for device in devices if device.strip() != '']
+    
+    return len(video_devices) - 1   # Subtract 1 to exclude the header line
+
+# Call the function and print the result
+usb_video_ports = get_usb_video_devices()
+print(f"Total USB cameras or video capture devices plugged in: {usb_video_ports}")
+
+cap = cv2.VideoCapture(i)
+
+if not cap.isOpened():
+    print(f"Error: Could not open device {i}")
+
+# Capture one frame
+ret, frame = cap.read()
+
+if ret:
+    image_path = "C:\\Users\\ivana\\Downloads\\"
