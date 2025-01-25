147acc411e56ca023d0c3d72b4b26badb6ae7c05
Russ <116739050+rrrusst@users.noreply.github.com>
2025-01-26 05:31:16 +0800

    Partial update of functions, and update of default config template
    Updated default config template. Default values of the options will be adjusted down the line (currently assigned with placeholder values).
    Updated the following functions:
    - init()
    - create_default_config()
    - validate_config() . This validates the sections, options, sub-options and sub-sub-options based on default config template.
    - save_config()
    
    Added the following functions:
    - is_fresh_setup() . This can be called to check the config.ini parameters and determine if a fresh setup is detected. Returns True if fresh setup is detected.

--- Testing/config_handler.py
+++ Testing/config_handler.py
@@ -20,7 +20,8 @@ class ConfigHandler:
             "volume": "0.5",
             "repeat": "1",
         }
-    }'''
+    }
+    '''
 
     DEFAULT_CONFIG = {
         "GUI": {
@@ -33,11 +34,10 @@ class ConfigHandler:
             "volume": "0.5",
             "repeat": "1",
         },
-        "Input Devices": {
-            "device0": {"usb_alt_name": "test alt name", "custom_name": "test custom name"}
-        },
-        "Triggers": {
-            "device0": {"keywords": ["keyword 1","keyword 2",], "tts_text": "hello", "bg_colour": "RED"}
+        "Input Device 0": {
+            "usb_alt_name": "",
+            "custom_name": "",
+            "triggers": {"cond0":{"keywords":["keyword 1","keyword 2",], "tts_text": "hello", "bg_colour": "RED"}},
         }
     }
 
@@ -49,70 +49,184 @@ class ConfigHandler:
         """Initializes the configuration: creates or validates the config file."""
         if not os.path.exists(ConfigHandler.CONFIG_FILE):
             ConfigHandler.create_default_config()
-            print("Config file created with default settings.")
+            print("No config.ini detected. New config.ini has been created with default settings.")
+            #ConfigHandler.is_fresh_setup() # TO REMOVE?
         else:
             try:
                 ConfigHandler.cp.read(ConfigHandler.CONFIG_FILE)
-                ConfigHandler.validateconfig()
             except Exception as e:
                 traceback.print_exc()
-                print(f"The config.ini file has been corrupted.")
-                ConfigHandler.validateconfig()
+                print("The config.ini file is corrupted. New config.ini has been created with default settings.")
+                ConfigHandler.create_default_config()    
+                #ConfigHandler.is_fresh_setup() # TO REMOVE?
+            ConfigHandler.validate_config()
+            ''' #### TO REMOVE?? ####
+            cfg_validity = ConfigHandler.validate_config()
+            if cfg_validity == False:
+                ConfigHandler.is_fresh_setup()
+            '''
 
     @staticmethod
     def create_default_config():
         """Creates a new config.ini file with default settings."""
+        for sections in ConfigHandler.cp.sections():
+            ConfigHandler.cp.remove_section(sections)
         for section, options in ConfigHandler.DEFAULT_CONFIG.items():
             ConfigHandler.cp[section] = options
-        ConfigHandler.saveconfig()
+        ConfigHandler.save_config()
 
     @staticmethod
-    def validateconfig():
+    def validate_config():
+        print("Validating config.ini...")
+        ### CHECK SECTIONS FOR VALIDITY
         cfg_file_sections = set(ConfigHandler.cp.sections())
         default_cfg_sections = set(ConfigHandler.DEFAULT_CONFIG.keys())
-        # Check if config.ini file's sections matches the sections in DEFAULT_CONFIG
-        if cfg_file_sections != default_cfg_sections:
-            diff_sections = cfg_file_sections.difference(default_cfg_sections)  # Check what sections the config.ini has that DEFAULT_CONFIG does not.
-            for val in diff_sections:
-                ConfigHandler.cp.remove_section(val)
-            
-            # If some sections are different = .ini file has different structure. Hence, need to overwrite with default config.ini.
+        
+        # Check if any sections in DEFAULT_CONFIG cannot be found in config.ini. If yes, condition is True.
+        if default_cfg_sections - cfg_file_sections:
+            print("Some config.ini sections are missing. New config.ini has been created with default settings.")
             ConfigHandler.create_default_config()
-            print("Some config.ini sections are different. New config.ini has been created with default settings.")
-
+            return False    # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.
+        # Check if any sections in config.ini cannot be found in DEFAULT_CONFIG. If yes, condition is True.
+        elif diff_sections := cfg_file_sections - default_cfg_sections:
+            for val in diff_sections:
+                if not val.startswith("Input Device "):
+                    print("Invalid section(s) found in config.ini sections. New config.ini has been created with default settings.")
+                    ConfigHandler.create_default_config()
+                    return False    # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.
         else:
-            # Check if the fixed keys in each applicable config.ini section matches DEFAULT_CONFIG
-            if (dict(ConfigHandler.cp.items("GUI")).keys() !=  ConfigHandler.DEFAULT_CONFIG["GUI"].keys()
-                or dict(ConfigHandler.cp.items("TTS Settings")).keys() !=  ConfigHandler.DEFAULT_CONFIG["TTS Settings"].keys()):
-                
-                # If some keys are not present = .ini file has different structure. Hence, need to overwrite with default config.ini.
-                ConfigHandler.create_default_config()
-                print("Some config.ini keys are MISSING. New config.ini has been created with default settings.")
-            else:
-                # All keys are present = .ini file has same structure. Hence, do nothing
-                print("All config.ini keys are PRESENT.")
-                
-            # Check if the format of values in dynamic keys for each applicable config.ini section matches DEFAULT_CONFIG
-            for cfg_options in dict(ConfigHandler.cp.items("Input Devices")).values():
-                for default_options in ConfigHandler.DEFAULT_CONFIG["Input Devices"].values():
-                    if ast.literal_eval(cfg_options).keys() != default_options.keys():
-                        ConfigHandler.create_default_config()
-                        print("Some config.ini values are MALFORMED. New config.ini has been created with default settings.")
-                        return  # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.
+            print("All sections are valid in config.ini.")
+
+        ### CHECK KEYS AND VALUE TYPES IN [GUI] AND [TTS SETTINGS] 
+        # Check if keys under [GUI] and [TTS Settings] in config.ini match DEFAULT_CONFIG
+        if (dict(ConfigHandler.cp.items("GUI")).keys() !=  ConfigHandler.DEFAULT_CONFIG["GUI"].keys()
+            or dict(ConfigHandler.cp.items("TTS Settings")).keys() !=  ConfigHandler.DEFAULT_CONFIG["TTS Settings"].keys()):
+            ConfigHandler.create_default_config()
+            print("Some config.ini keys are missing or invalid. New config.ini has been created with default settings.")
+            return False    # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.
+        # Check if their value types in config.ini match DEFAULT_CONFIG
+        else:
+            for key, val in dict(ConfigHandler.cp.items("GUI")).items():
+                try:
+                    cfg_val_type = type(ast.literal_eval(val))
+                except (ValueError, SyntaxError):
+                    cfg_val_type = type(val)
+                except:
+                    traceback.print_exc()
                     
-            # Check if the format of values in dynamic keys for each applicable config.ini section matches DEFAULT_CONFIG                        
-            for cfg_options in dict(ConfigHandler.cp.items("Triggers")).values():
-                for default_options in ConfigHandler.DEFAULT_CONFIG["Triggers"].values():
-                    if ast.literal_eval(cfg_options).keys() != default_options.keys():
+                try:
+                    default_val_type = type(ast.literal_eval(ConfigHandler.DEFAULT_CONFIG["GUI"][key]))
+                except (ValueError, SyntaxError):
+                    default_val_type = type(ConfigHandler.DEFAULT_CONFIG["GUI"][key])
+                except:
+                    traceback.print_exc()     
+            
+                if cfg_val_type != default_val_type:
+                    print("Invalid value type found under [GUI] in config.ini. New config.ini has been created with default settings.")
+                    ConfigHandler.create_default_config()
+                    return False    # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.
+                        
+            for key, val in dict(ConfigHandler.cp.items("TTS Settings")).items():
+                try:
+                    cfg_val_type = type(ast.literal_eval(val))
+                except (ValueError, SyntaxError):
+                    cfg_val_type = type(val)
+                except:
+                    traceback.print_exc()
+                    
+                try:
+                    default_val_type = type(ast.literal_eval(ConfigHandler.DEFAULT_CONFIG["TTS Settings"][key]))
+                except (ValueError, SyntaxError):
+                    default_val_type = type(ConfigHandler.DEFAULT_CONFIG["TTS Settings"][key])
+                except:
+                    traceback.print_exc()
+
+                if cfg_val_type != default_val_type:
+                    print("Invalid value type found under [TTS Settings] in config.ini. New config.ini has been created with default settings.")
+                    ConfigHandler.create_default_config()
+                    return False    # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.
+            print("[GUI] and [TTS Settings] sections, keys and value types are valid.")        
+
+        ### CHECK KEYS AND VALUE TYPES IN [INPUT DEVICE X]
+        # Loop through every section in config.ini that starts with "Input Device "
+        for section in (temp for temp in ConfigHandler.cp.sections() if temp.startswith("Input Device ")):
+            # Check if the keys can be found in DEFAULT_CONFIG.
+            if dict(ConfigHandler.cp.items(section)).keys() !=  ConfigHandler.DEFAULT_CONFIG["Input Device 0"].keys():
+                ConfigHandler.create_default_config()
+                print("Some config.ini keys are missing or invalid. New config.ini has been created with default settings.")
+                return False    # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.
+            # Check if the type of dict values in "triggers" key in config.ini match DEFAULT_CONFIG.
+            # No need to check "usb_alt_name" and "custom_name" values since both are just Strings.
+            else:
+                try:
+                    cfg_triggers_dict = ast.literal_eval(ConfigHandler.cp[section]["triggers"])
+                    if not isinstance(cfg_triggers_dict, dict):
+                        raise ValueError
+                except (ValueError, SyntaxError):
+                    print(f"Invalid value type found under [{section}]-triggers in config.ini. New config.ini has been created with default settings.")
+                    ConfigHandler.create_default_config()
+                    return False    # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.                  
+                except:
+                    traceback.print_exc()
+
+                # Set a temp dict with the sub-value (dict) of "triggers" to make type comparison less complicated later
+                default_triggers_dict = {}
+                for sub_val in ConfigHandler.DEFAULT_CONFIG["Input Device 0"]["triggers"].values():
+                    default_triggers_dict = sub_val
+
+                # Loop through all items in cfg_triggers_dict
+                for sub_key, sub_val in cfg_triggers_dict.items():
+                    # Check if inner dict keys in cfg_triggers_dict match the inner dict keys in default_triggers_dict
+                    if sub_val.keys() != default_triggers_dict.keys():
+                        print(f"Invalid sub-value key found under [{section}]-triggers in config.ini. New config.ini has been created with default settings.")
                         ConfigHandler.create_default_config()
-                        print("Some config.ini values are MALFORMED. New config.ini has been created with default settings.")
-                        return  # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.
+                        return False    # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.
+                    else:
+                        # Loop through all items in the inner dict of cfg_triggers_dict
+                        for cfg_sub_sub_key, cfg_sub_sub_val in sub_val.items():
+                            # Loop through all items in inner dict of default_triggers_dict
+                            for default_sub_sub_key, default_sub_sub_val in default_triggers_dict.items():
+                                if cfg_sub_sub_key == default_sub_sub_key:
+                                    if type(cfg_sub_sub_val) != type(default_sub_sub_val):
+                                        print(f"Invalid sub-value type found under [{section}]-triggers in config.ini. New config.ini has been created with default settings.")
+                                        ConfigHandler.create_default_config()
+                                        return False    # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.                  
+        print("[Input Device X] sections, keys and value types are valid.")
+        
+        # If function has run up till this point, then config.ini is fully validated. Hence, return True.
+        print("Result: Config.ini is valid.")
+        return True
+            
     @staticmethod
-    def saveconfig():
+    def save_config():
         """Saves the current config to the file."""
         with open(ConfigHandler.CONFIG_FILE, "w", encoding = "utf-8") as file:
             ConfigHandler.cp.write(file)
 
+    #DONE
+    @staticmethod
+    def is_fresh_setup():
+        """Returns True if it detects a fresh setup (fresh setup requires user to be brought straight to setup page)"""
+        for section in (temp for temp in ConfigHandler.cp.sections() if temp.startswith("Input Device ")):
+            cfg_usb_alt_name = dict(ConfigHandler.cp.items(section))["usb_alt_name"]
+            try:
+                cfg_triggers_dict = ast.literal_eval(ConfigHandler.cp[section]["triggers"])
+                if not isinstance(cfg_triggers_dict, dict):
+                    raise ValueError
+            except (ValueError, SyntaxError):
+                print(f"Invalid value type found under [{section}]-triggers in config.ini. New config.ini has been created with default settings.")
+                ConfigHandler.create_default_config()
+                return False    # Overwriting with default config only needs to happen once, and checks on remaining sections/options are not needed.                  
+            except:
+                traceback.print_exc()
+
+            # Check if the usb_alt_name is empty/spaces, and if the triggers in config.ini matches DEFAULT_CONFIG. This is a sign that user has not set up ADAM yet.
+            if (cfg_usb_alt_name == "" or cfg_usb_alt_name.isspace()) and (cfg_triggers_dict == ConfigHandler.DEFAULT_CONFIG[section]["triggers"]):
+                print("Fresh setup detected. Bringing you to the setup page.")
+                return True
+            else:
+                return False
+    
     # TODO: Is this func really needed? Maybe just use get_triggers(), which returns a dict? Then use ast.literal_eval(key) to get the values into dict format.
     @staticmethod
     def get_list(section, option):
@@ -127,7 +241,7 @@ class ConfigHandler:
         if section not in ConfigHandler.cp:
             ConfigHandler.cp[section] = {}
         ConfigHandler.cp[section][option] = ",".join(values)
-        ConfigHandler.saveconfig()
+        ConfigHandler.save_config()
 
     # TODO: Is this func really needed? Can just use set_value()
     @staticmethod
