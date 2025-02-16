0ac2f5ea3e62323ba9a22cc676eb0b24e8119b59
ivanaitzliddat <ivanaitzliddat@gmail.com>
2025-02-16 16:10:43 +0800

    Update example usage of ProcessedScreenshot

--- Actual/processed_screenshot.py
+++ Actual/processed_screenshot.py
@@ -5,16 +5,15 @@ class Processed_Screenshot:
     lock = threading.Lock()
 
 # def upon_receive_message():
-#     screenshot, ttsmessage = Processed_Screenshot.frames['device1']['timestamp1']
 #     TTS.say(ttsmessage)
     
 # def on_click():
-#     screenshot, ttsmessage = Processed_Screenshot.frames['device1']['timestamp1']
+#     screenshot = Processed_Screenshot.frames['device1']['timestamp1']
 #     display(screenshot)
 
 # def ocr_identified():
-#     Processed_Screenshot.frames.update('device1', 'timestamp1', ('screenshot3', 'ttsmessage2'))
+#     Processed_Screenshot.frames.update('device1', 'timestamp1', 'screenshot3')
 
-# frames = {'device1': {'timestamp1':('screenshot1', 'ttsmessage1'), 'timestamp2':('screenshot2', 'ttsmessage2')},
-#           'device2': {'timestamp1':('screenshot1', 'ttsmessage1')},
-#           'device3': {'timestamp1':('screenshot1', 'ttsmessage2'), 'timestamp2':('screenshot2', 'ttsmessage2'), 'timestamp3': ('screenshot3', 'ttsmessage3')}}
+# frames = {'device1': {'timestamp1':'screenshot1', 'timestamp2':'screenshot2'},
+#           'device2': {'timestamp1':'screenshot1'},
+#           'device3': {'timestamp1':'screenshot1', 'timestamp2':'screenshot2', 'timestamp3':'screenshot3'}}
